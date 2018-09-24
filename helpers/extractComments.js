70bb68cd601ce89b316658e578a7cc5255714029
Joanna Kao <joanna.kao@ft.com>
2018-09-24 16:52:50 +0100

    Attempt to get sentiment from comments TODO: fix promises/callback issue

--- helpers/extractComments.js
+++ helpers/extractComments.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 
-function getCommentsFromUUID(uuid) {
+module.exports = function getCommentsFromUUID(uuid) {
   const commentsList = [];
-  fetch(`http://comment-creation.webservices.ft.com/v1/getComments?articleId=${uuid}&url=https%3A%2F%2Fwww.ft.com%2Fcontent%2F${uuid}&title=test`)
+  return fetch(`http://comment-creation.webservices.ft.com/v1/getComments?articleId=${uuid}&url=https%3A%2F%2Fwww.ft.com%2Fcontent%2F${uuid}&title=test`)
   .then(d => d.json())
   .then(d => {
     for (let i = 0; i < d.collection.comments.length; i ++) {
---
+++ helpers/getCommentSentiment.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+require('dotenv').config()
+const extractComments = require("./extractComments");
+const extractText = require("./extractText");
+
+var comprehend = new AWS.Comprehend({
+  apiVersion: '2017-11-27',
+  region: 'eu-west-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+async function getSentiment(textList) {
+  return comprehend.batchDetectSentiment({
+    LanguageCode: 'en',
+    TextList: textList,
+  }, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      console.log('hi', data);           // successful response
+      return data;
+    }
+  });
+}
+
+async function getCommentSentiment(uuid) {
+  const comments = await extractComments(uuid);
+  const commentsText = comments.map(c => extractText(c)).slice(0, 25);
+  const sentiment = await getSentiment(commentsText);
+  console.log('sentiment', sentiment);
+  return sentiment;
+}
+
+getCommentSentiment('fc7ae0e4-bfc3-11e8-95b1-d36dfef1b89a');
