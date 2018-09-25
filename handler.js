5277d8d922643b2ad8c165d49fcbb35b19d7ae12
Jamie.Brown <jamiebrown201@hotmail.com>
2018-09-25 09:36:14 +0100

    bringing together all data points

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 ("use strict");
 const article = require("./helpers/article");
 const topicExtraction = require("./helpers/topicExtraction");
+const sentiment = require("./helpers/getCommentSentiment");
 
 module.exports.main = async (event, context, callback) => {
   const data = event.queryStringParameters;
@@ -9,12 +10,13 @@ module.exports.main = async (event, context, callback) => {
     try {
       const articleText = await article.getArticleText(data.uuid);
       const topic = await topicExtraction.topicExtraction(articleText.text);
-      console.log(topic);
-      // console.log("topic", topic.data);
+      const commentSentiment = await sentiment.getCommentSentiment(data.uuid);
+      console.log(commentSentiment);
+
       const response = {
         statusCode: 200,
         body: JSON.stringify({
-          message: "hello",
+          message: { topic, commentSentiment },
           input: event
         })
       };
--- helpers/getCommentSentiment.js
+++ helpers/getCommentSentiment.js
@@ -1,28 +1,29 @@
-const AWS = require('aws-sdk');
-require('dotenv').config()
+const AWS = require("aws-sdk");
+require("dotenv").config();
 const extractComments = require("./extractComments");
 const extractText = require("./extractText");
 
 var comprehend = new AWS.Comprehend({
-  apiVersion: '2017-11-27',
-  region: 'eu-west-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  apiVersion: "2017-11-27",
+  region: "eu-west-1"
 });
 
 async function getSentiment(textList) {
   return new Promise((resolve, reject) => {
-    comprehend.batchDetectSentiment({
-      LanguageCode: 'en',
-      TextList: textList,
-    }, function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-        reject();
-      } else {
-        resolve(data);
+    comprehend.batchDetectSentiment(
+      {
+        LanguageCode: "en",
+        TextList: textList
+      },
+      function(err, data) {
+        if (err) {
+          console.log(err, err.stack); // an error occurred
+          reject();
+        } else {
+          resolve(data);
+        }
       }
-    });
+    );
   });
 }
 
@@ -30,8 +31,7 @@ async function getCommentSentiment(uuid) {
   const comments = await extractComments(uuid);
   const commentsText = comments.map(c => extractText(c)).slice(0, 25);
   const sentiment = await getSentiment(commentsText);
-  console.log(sentiment);
   return sentiment;
 }
 
-getCommentSentiment('fc7ae0e4-bfc3-11e8-95b1-d36dfef1b89a');
+module.exports = { getCommentSentiment };
