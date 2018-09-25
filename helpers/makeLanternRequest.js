1d686c46cee74d51c1095dc8751a7c1576532b14
Jamie.Brown <jamiebrown201@hotmail.com>
2018-09-25 09:36:56 +0100

    format lantern request

--- helpers/makeLanternRequest.js
+++ helpers/makeLanternRequest.js
@@ -1,17 +1,20 @@
 const fetch = require("node-fetch");
-require('dotenv').config()
+require("dotenv").config();
 
 function lanternApiRequest(uuid) {
   const options = {
     method: "GET",
     headers: {
-      "x-api-key": process.env.LANTERN_API_KEY,
+      "x-api-key": process.env.LANTERN_API_KEY
     }
   };
 
-  return fetch(`https://api-lantern.ft.com/realtime/articles?uuid=${uuid}&timespan=48h`, options)
+  return fetch(
+    `https://api-lantern.ft.com/realtime/articles?uuid=${uuid}&timespan=48h`,
+    options
+  )
     .then(d => d.json())
     .then(d => d);
 }
 
-lanternApiRequest('fc7ae0e4-bfc3-11e8-95b1-d36dfef1b89a');
+module.exports = lanternApiRequest;
